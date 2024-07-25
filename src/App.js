4a512dc1146e396db95fe210528092d777d4b96c
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-07-25 12:56:36 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
+import TodoForm from './components/TodoForm.jsx'
+import TodoList from './components/TodoForm.jsx'
+import React from 'react';
+import './App.css'
 
+function App() {
+  const {tasks, setTasks} = useState([]);
+
+  useEffect(()=> {console.log('ToDo List Loaded!')}, []);
+
+  const addTask = (task) => {
+    setTasks([...tasks, {text: task, completed: false}])
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const deleteTasks = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className='App'>
+      <h1>ToDo List v0.0000001</h1>
+      <TodoForm addTask={addTask}/>
+      <TodoList tasks={tasks} completeTask={completeTask} deleteTask={deleteTasks} />
+    </div>
+  );
+};
+
+export default App;
